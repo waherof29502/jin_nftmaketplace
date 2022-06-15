22e42935fe25c49b8aa6f32f8f2270a67598fab6
JinPing <39645652+waherof29502@users.noreply.github.com>
2022-06-15 09:16:54 +0800

    code structure

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { ThemeProvider } from 'next-themes';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider attribute='class'>
+      <div className='dark:bg-nft-dark'>
+        <h1>NavBar</h1>
+        <Component {...pageProps} />
+        <h1>Footer</h1>
+      </div>
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
