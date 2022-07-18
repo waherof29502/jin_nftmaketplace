0cb1f4959bc476626022f26782aee65566447173
JinPing <39645652+waherof29502@users.noreply.github.com>
2022-07-18 15:20:58 +0800

    added listednft

--- pages/created-nfts.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const CreatedNFTs = () => {
-  return <div>CreateNFT</div>;
-};
-
-export default CreatedNFTs;
--- pages/my-nfts.jsx
+++ pages/my-nfts.jsx
@@ -1,7 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
+import Image from 'next/image';
+
+import { NFTContext } from '../context/NFTContext';
+import { Loader, NFTCard, Banner } from '../components';
+
+import images from '../assets';
 
 const MyNFTs = () => {
-  return <div>MyNFT</div>;
+  const { fetchMyNFTsOrFetchListedNFTs, currentAccount } =
+    useContext(NFTContext);
+  useContext(NFTContext);
+  const [nfts, setNfts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  if (isLoading) {
+    return (
+      <div className='flexStart min-h-screen'>
+        <Loader />
+      </div>
+    );
+  }
+
+  return (
+    <div className='w-full flex justify-start items-center flex-col min-h-screen'>
+      <div className='w-full flexCenter flex-col'>
+        <Banner
+          name='Your Nifty NFTs'
+          childStyles='text-center mb-4'
+          parentStyles='h-80 justify-center'
+        />
+      </div>
+    </div>
+  );
 };
 
 export default MyNFTs;
