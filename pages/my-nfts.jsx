7f2b334c8c050283fd07cd448265695b63ecece8
JinPing <39645652+waherof29502@users.noreply.github.com>
2022-07-19 09:12:28 +0800

    my nft page

--- pages/my-nfts.jsx
+++ pages/my-nfts.jsx
@@ -5,6 +5,7 @@ import { NFTContext } from '../context/NFTContext';
 import { Loader, NFTCard, Banner } from '../components';
 
 import images from '../assets';
+import { shortenAddress } from '../utils/shortenAddress';
 
 const MyNFTs = () => {
   const { fetchMyNFTsOrFetchListedNFTs, currentAccount } =
@@ -29,7 +30,39 @@ const MyNFTs = () => {
           childStyles='text-center mb-4'
           parentStyles='h-80 justify-center'
         />
+        <div className='flexCenter flex-col -mt-20 z-0'>
+          <div className='flexCenter w-40 h-40 sm:w-36 sm:h-36 p-1 bg-nft-black-2 rounded-full'>
+            <Image
+              src={images.creator1}
+              className='rounded-full object-cover'
+              objectFit='cover'
+            />
+          </div>
+          <p className='font-poppins dark:text-white text-nft-black-1 font-semibold text-2xl mt-6'>
+            {shortenAddress(currentAccount)}
+          </p>
+        </div>
       </div>
+
+      {!isLoading && !nfts.length ? (
+        <div className='flexCenter sm:p-4 p-16'>
+          <h1 className='font-poppins dark:text-white text-nft-black-1 font-extrabold text-3xl'>
+            No NFTs Owned
+          </h1>
+        </div>
+      ) : (
+        <div className='sm:px-4 p-12 w-fll minmd:w-4/5 flexCenter flex-col'>
+          <div className='flex-1 w-full flex flex-row sm:flex-col px-4 xs:px-0 minlg:px-8'>
+            SearchBar
+          </div>
+
+          <div className='mt-3 w-full flex flex-wrap'>
+            {nfts.map((nft) => (
+              <NFTCard key={nft.token} nft={nft} />
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
